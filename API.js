496ae3b684c5bde35379a1bfdc0418f99c060f57
KINGALVI <alvialmahamud@gmail.com>
2024-03-07 03:14:45 +0600

    edit some code

--- API.js
+++ API.js
@@ -33,3 +33,35 @@ console.log(parseByJSON);
 
 
 
+// JAVASCRIPT এ API Data কে ব্যবহার করার জন্য মূলত দুই রকমের fetch Operation করা হয় । নিচে আমার তার উধাহরণ দেওয়া হল -
+
+
+// ১ । এটি হলো Normal fetch , যেখানে একসাথে Operation হয়ে থাকে । 
+
+const loadUserFetch = () => {
+    const url = 'https://randomuser.https://randomuser.me/api/?gender-female';
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayUser(data.results[0]))
+        .catch(error => console.log(error));
+}
+
+// ২ । এটি হলো await fetch , যেখানে একটি পর একটি Opreration হয়ে থাকে । প্রথমে fetch এর Operation হয়ে থাকে । তারপর fetch Operation থেকে যে Responce পাওয়া যায় , সেই Responce কে json এ Convert করা হয় । এরপর সব শেষে API এর Data কে Show করা হয় । নিচে আমার তার উধাহরণ দেওয়া হল - 
+
+const loadUserAsync = async () => {
+    const url = 'https://randomuser.https://randomuser.me/api/?gender-female';
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayUser(data.results[0]);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+//
+
+const displayUser = User => {
+    console.log(User);
+}
--- alvi.js
+++ alvi.js
@@ -245,7 +245,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
     });
 
-
 });
 
 
