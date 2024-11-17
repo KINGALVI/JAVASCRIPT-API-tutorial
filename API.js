95cdcd288294bae410d6eaab61eb446c8acf7c16
KING ALVI <94359754+KINGALVI@users.noreply.github.com>
2024-11-17 04:33:58 +0200

    Update API.js

--- API.js
+++ API.js
@@ -1,4 +1,4 @@
-// JAVASCRIPT API এর JSON Data কে ব্যবহার করতে হলে fetch ব্যবহার করতে হবে । নিচে তার উধাহরন দেওয়া হলো - 
+// ১। JAVASCRIPT API এর JSON Data কে ব্যবহার করতে হলে fetch ব্যবহার করতে হবে । নিচে তার উধাহরন দেওয়া হলো - 
 
 document.getElementById('API-Button').addEventListener('click', function () {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
@@ -10,7 +10,7 @@ document.getElementById('API-Button').addEventListener('click', function () {
 
 
 
-// JAVASCRIPT এর যেকোন Array , Object বা Value কে JSON এর String এ convert করার জন্য JSON.stringify ব্যাবহার করা হয় । নিচে তার উধাহরন দেওয়া হলো -
+// ২। JAVASCRIPT এর যেকোন Array , Object বা Value কে JSON এর String এ convert করার জন্য JSON.stringify ব্যাবহার করা হয় । নিচে তার উধাহরন দেওয়া হলো -
 
 const Data1 = { Id: 1, Name: "KING ALVI", Job: "Bisnessman" };
 console.log(Data1);
@@ -21,7 +21,7 @@ console.log(stringifyByJSON1);
 
 
 
-// JAVASCRIPT এর যেকোন Array , Object বা Value কে JSON এর String থেকে পুনুরায় JAVASCRIPT এর Array , Object বা Value তে রুপান্তর করার জন্য JSON.parse ব্যবহার করা হয় । নিচে তার উধাহরন দেওয়া হলো - 
+// ৩। JAVASCRIPT এর যেকোন Array , Object বা Value কে JSON এর String থেকে পুনুরায় JAVASCRIPT এর Array , Object বা Value তে রুপান্তর করার জন্য JSON.parse ব্যবহার করা হয় । নিচে তার উধাহরন দেওয়া হলো - 
 
 const Data2 = { Id: 1, Name: "KING ALVI", Job: "Bisnessman" };
 const stringifyByJSON2 = JSON.stringify(Data2);
@@ -33,10 +33,10 @@ console.log(parseByJSON);
 
 
 
-// JAVASCRIPT এ API Data কে ব্যবহার করার জন্য মূলত দুই রকমের fetch Operation করা হয় । নিচে আমার তার উধাহরণ দেওয়া হল -
+// ৪। JAVASCRIPT এ API Data কে ব্যবহার করার জন্য মূলত দুই রকমের fetch Operation করা হয় । নিচে আমার তার উধাহরণ দেওয়া হল -
 
 
-// ১ । এটি হলো Normal fetch , যেখানে একসাথে Operation হয়ে থাকে । 
+//// ১ । এটি হলো Normal fetch , যেখানে একসাথে Operation হয়ে থাকে । 
 
 const loadUserFetch = () => {
     const url = 'https://randomuser.https://randomuser.me/api/?gender-female';
@@ -46,7 +46,7 @@ const loadUserFetch = () => {
         .catch(error => console.log(error));
 }
 
-// ২ । এটি হলো await fetch , যেখানে একটি পর একটি Opreration হয়ে থাকে । প্রথমে fetch এর Operation হয়ে থাকে । তারপর fetch Operation থেকে যে Responce পাওয়া যায় , সেই Responce কে json এ Convert করা হয় । এরপর সব শেষে API এর Data কে Show করা হয় । নিচে আমার তার উধাহরণ দেওয়া হল - 
+//// ২ । এটি হলো await fetch , যেখানে একটি পর একটি Opreration হয়ে থাকে । প্রথমে fetch এর Operation হয়ে থাকে । তারপর fetch Operation থেকে যে Responce পাওয়া যায় , সেই Responce কে json এ Convert করা হয় । এরপর সব শেষে API এর Data কে Show করা হয় । নিচে আমার তার উধাহরণ দেওয়া হল - 
 
 const loadUserAsync = async () => {
     const url = 'https://randomuser.https://randomuser.me/api/?gender-female';
@@ -60,8 +60,8 @@ const loadUserAsync = async () => {
     }
 }
 
-//
 
-const displayUser = User => {
-    console.log(User);
-}
+
+
+
+                                              // advance JAVASCRIPT Toturial END //
