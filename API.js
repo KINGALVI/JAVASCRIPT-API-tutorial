63fe0b7e6005450cf2a256330020484c2b48973c
KING ALVI <94359754+KINGALVI@users.noreply.github.com>
2024-11-20 23:28:39 +0200

    Update API.js

--- API.js
+++ API.js
@@ -9,7 +9,7 @@ document.getElementById('API-Button').addEventListener('click', function () {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
         .then(response => response.json())
         .then(json => console.log(json))
-})
+}) 
 
 
 
